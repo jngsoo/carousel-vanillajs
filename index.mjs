b2a28b29dc3d9b3a74817e814863525b7f3c8712
jngsoo <wt2933@icloud.com>
2019-09-20 10:24:03 +0900

    Add demo carousel data
    JSON format

--- index.mjs
+++ index.mjs
@@ -5,26 +5,26 @@ const cards = [
         "carousel" : [
             {
                 "img" : "https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE1",
-                "content" : "hasdjfk",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "category_name" : "FAST, FREE DELIVERY",
+                "title" : "Fast, FREE delivery on over 100 million items",
+                "content" : "Super-fast delivery, tens of millions of items, and flexible delivery options to fit your life. Plus, Prime members get FREE One-Day Delivery on tens of millions of items.",
+                "detail_comment" : "Explore Prime Delivery &#9656;",
                 "detail_href" : "http://www.google.com"
             },
             {
                 "img" : "https://images.unsplash.com/photo-1553413077-190dd305871c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE2",
-                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "category_name" : "SAME-DAY DELIVERY",
+                "title" : "FREE Same-Day Delivery",
+                "content" : "Prime members get FREE Same-Day Delivery on over three million items with qualifying orders.",
+                "detail_comment" : "Explore Same-Day Delivery &#9656;",
                 "detail_href" : "http://www.google.com"
             },
             {
                 "img" : "https://images.unsplash.com/photo-1455541504462-57ebb2a9cec1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE3",
-                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "category_name" : "RELEASE DAY DELIVERY",
+                "title" : "Skip the lines and get it on release day",
+                "content" : "No more waiting in line! Choose Free Release-Date Delivery at checkout on qualified items, and your package will be delivered on the release day by 7pm.",
+                "detail_comment" : "Explore Release-Date Delivery &#9656;",
                 "detail_href" : "http://www.google.com"
             }
         ]
@@ -34,11 +34,35 @@ const cards = [
         "title" : "Stream",
         "carousel" : [
             {
-                "img" : "https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE4",
-                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "img" : "https://images.unsplash.com/photo-1462393582935-1ac76b85dcf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+                "category_name" : "PRIME VIDEO",
+                "title" : "Watch movies, TV, live events, and more",
+                "content" : "As a Prime member, you can watch exclusive Amazon Originals and thousands of popular movies and TV shows—all at no extra cost. Watch at home or on the go with practically any device.",
+                "detail_comment" : "Explore Prime Video &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
+                "img" : "https://images.unsplash.com/photo-1527334919515-b8dee906a34b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+                "category_name" : "TWITCH PRIME",
+                "title" : "Free Games & Loot with Twitch Prime",
+                "content" : "Gamers can get free games, in-game items, a free Twitch channel subscription every month and more with Twitch Prime.",
+                "detail_comment" : "Explore Twitch Prime &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
+                "img" : "https://images.unsplash.com/photo-1511367734837-f2956f0d8020?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1894&q=80",
+                "category_name" : "PRIME MUSIC",
+                "title" : "Millions of songs for every moment",
+                "content" : "As a Prime member, you can stream over 2 million songs ad free, listen on any Echo device, and take your music anywhere with offline listening.",
+                "detail_comment" : "Explore Prime Music &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
+                "img" : "https://images.unsplash.com/photo-1512645592367-97ba8a9d4035?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1938&q=80",
+                "category_name" : "AUDIBLE CHANNELS",
+                "title" : "Original audio programs for life on the go",
+                "content" : "Audible Channels are free with your Prime membership. Enjoy original audio series and playlists handcrafted for every interest. Just download the Audible app and start listening.",
+                "detail_comment" : "Explore Audible Channels &#9656;",
                 "detail_href" : "http://www.google.com"
             }
         ]
@@ -48,11 +72,27 @@ const cards = [
         "title" : "Shop",
         "carousel" : [         
             {
+                "img" : "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
+                "category_name" : "PRIME AT WHOLE FOODS MARKET",
+                "title" : "Our healthiest benefit yet",
+                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
+                "detail_comment" : "Explore Prime at Whole Foods &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
+                "img" : "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80",
+                "category_name" : "ALEXA VOICE SHOPPING",
+                "title" : "The simplest way to shop. Just ask Alexa.",
+                "content" : "With Alexa, shopping for essentials and reordering your favorite items from Amazon has never been easier.",
+                "detail_comment" : "Learn more about Alexa and Prime &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
                 "img" : "https://images.unsplash.com/photo-1556910101-a533e487de15?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE5",
-                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "category_name" : "JUST WITH PRIME",
+                "title" : "Exclusive brands and deals available only to Prime members",
+                "content" : "As a Prime member enjoy early access to deals, Prime exclusive brands, and savings.",
+                "detail_comment" : "Shop Prime Member Exclusives &#9656;",
                 "detail_href" : "http://www.google.com"
             }
         ]
@@ -63,10 +103,18 @@ const cards = [
         "carousel" : [         
             {
                 "img" : "https://images.unsplash.com/photo-1568750603117-f26c65ac27c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE6",
-                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "category_name" : "PRIME READING",
+                "title" : "Prime members read free",
+                "content" : "As a Prime member, you can now read as much as you like from over a thousand top Kindle books, magazines, short works, books with Audible narration, comics, children's books and more—all at no additional cost.",
+                "detail_comment" : "Explore Prime Reading &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
+                "img" : "https://images.unsplash.com/photo-1556745757-8d76bdb6984b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1932&q=80",
+                "category_name" : "AMAZON FIRST READS",
+                "title" : "Read next month's new releases today",
+                "content" : "Each month, Prime members can download one editors' pick for free--before the official publication date. Downloaded titles are yours to keep.",
+                "detail_comment" : "Learn more about Amazon First Reads &#9656;",
                 "detail_href" : "http://www.google.com"
             }
         ]
@@ -77,10 +125,18 @@ const cards = [
         "carousel" : [         
             {
                 "img" : "https://images.unsplash.com/photo-1533315318720-a6538dcefcae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "category_name" : "PRIME ABCD EFG",
-                "title" : "THIS IS TITLE7",
-                "content" : "Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)Exclusive savings for Prime members in all US stores, 5% Back for eligible Prime members with the Amazon Prime Rewards Visa Card, and 2-hour delivery with Prime Now in select cities (more soon)",
-                "detail_comment" : "Learn more about Prime Rewards",
+                "category_name" : "5% BACK",
+                "title" : "Earn more with Prime Rewards",
+                "content" : "Eligible Prime members can earn 5% back at Amazon.com using the Amazon Prime Rewards Visa Card or the Amazon Prime Store card. All Prime members earn 2% rewards with Amazon Prime Reload.",
+                "detail_comment" : "Learn more about Prime Rewards &#9656;",
+                "detail_href" : "http://www.google.com"
+            },
+            {
+                "img" : "https://images.unsplash.com/photo-1556742212-5b321f3c261b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+                "category_name" : "AMAZON PHOTOS",
+                "title" : "Unlimited, free photo storage",
+                "content" : "Back up and share your photos with unlimited photo storage. Add your photos to the free app to see them on all your devices.",
+                "detail_comment" : "Explore Amazon Photos &#9656;",
                 "detail_href" : "http://www.google.com"
             }
         ]
@@ -90,7 +146,7 @@ const cards = [
 import Card from './components/Card.mjs'
 import Slide from './components/Slide.mjs'
 
-const transTime = 150
+const transTime = 300
 const $ = element => document.querySelector(element)
 const $All = element => document.querySelectorAll(element)
 let numOfSlides = 0
@@ -129,17 +185,9 @@ const nextBtn = $('.next')
 let page = 0
 let dotIndex = 0
 
-const expandCard = card => {
-    card.childNodes.forEach ( elem => {
-        if (elem.tagName === "SPAN") {
-            moveSlide(elem.childNodes[1].id)
-        }
-        // elem.style.width = "12rem"
-        // elem.style.height = '18rem'
-        // elem.style.marginRight = "1rem"
-        // elem.style.marginBottom = "1rem"
-    })
-
+const expandCard = (card,move) => {
+    if(move!==false) moveSlide(card.childNodes[5].childNodes[1].id)
+    card.style.transition = `${transTime*2}ms`
     card.style.width = "12rem"
     card.style.height = '18rem'
     card.style.marginRight = "1rem"
@@ -167,6 +215,9 @@ const slideNext = () => {
         slide.style.transition = `${transTime}ms`
         slide.style.margin = `0rem 1rem 0rem ${page*100}%`
         dots[dotIndex].className = 'dot-selected'
+        shrinkCard(clickedCard)
+        expandCard(dots[dotIndex].parentNode.parentNode, false)
+        clickedCard = dots[dotIndex].parentNode.parentNode
         console.log(page)
 
         // 마지막 슬라이드에 그려진 첫번째 슬라이드를 지우면서 첫번째 슬라이드로 실제 이동(transition 안보이게)
@@ -178,7 +229,6 @@ const slideNext = () => {
             slide.style.transition = "hidden"
             slide.style.margin = `0rem 1rem 0rem ${page*100}%`
             parent.removeChild(lastSlide);
-            console.log('im in first')
         },transTime)
         return
     }
@@ -188,6 +238,10 @@ const slideNext = () => {
     slide.style.transition = `${transTime}ms`
     slide.style.margin = `0rem 1rem 0rem ${page*100}%`
     dots[dotIndex].className = 'dot-selected'
+    shrinkCard(clickedCard)
+    expandCard(dots[dotIndex].parentNode.parentNode, false)
+    clickedCard = dots[dotIndex].parentNode.parentNode
+    
     console.log(page)
 }
 const slidePrev = () => {
@@ -203,22 +257,24 @@ const slidePrev = () => {
 
         page++   // 추가된 마지막 슬라이드(첫 슬라이드와 동일하게 생긴)로 진행방향대로 밀고
         dots[dotIndex].className = 'dot'
-        dotIndex = 6
+        dotIndex = numOfSlides-1
         slide.style.transition = `${transTime}ms`
         slide.style.margin = `0rem 1rem 0rem 0%`
         dots[dotIndex].className = 'dot-selected'
+        shrinkCard(clickedCard)
+        expandCard(dots[dotIndex].parentNode.parentNode, false)
+        clickedCard = dots[dotIndex].parentNode.parentNode
         console.log(page)
 
         // 마지막 슬라이드에 그려진 첫번째 슬라이드를 지우면서 첫번째 슬라이드로 실제 이동(transition 안보이게)
         let slideList = document.getElementsByTagName('li')
         const firstSlide = slideList[0]
         const parent = firstSlide.parentNode
-        page = -6
+        page = -(numOfSlides-1)
         setTimeout( () => {
             slide.style.transition = "hidden"
             slide.style.margin = `0rem 1rem 0rem ${page*100}%`
             parent.removeChild(firstSlide);
-            console.log('im in last')
         },transTime)
         return
     }
@@ -228,6 +284,9 @@ const slidePrev = () => {
     slide.style.transition = `${transTime}ms`
     slide.style.margin = `0rem 1rem 0rem ${page*100}%`
     dots[dotIndex].className = 'dot-selected'
+    shrinkCard(clickedCard)
+    expandCard(dots[dotIndex].parentNode.parentNode, false)
+    clickedCard = dots[dotIndex].parentNode.parentNode
     console.log(page)
 }
 
@@ -243,9 +302,12 @@ const moveSlide = index => {
 
 nextBtn.addEventListener('click', slideNext)
 prevBtn.addEventListener('click', slidePrev)
+
 cardContainer.addEventListener('click', e => {
-    
     if(e.target.tagName === "SPAN") {
+        shrinkCard(clickedCard)
+        expandCard(e.target.parentNode.parentNode)
+        clickedCard = e.target.parentNode.parentNode
         moveSlide(+e.target.id)
         console.log(page)
     }
